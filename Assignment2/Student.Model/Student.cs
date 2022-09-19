4946a202f0b66b23ddb5cd73ca44bdd58d5b1941
nsel <nsel@itu.dk>
2022-09-19 22:41:11 +0200

    added overloaded tostring

--- Assignment2/Student.Model/Student.cs
+++ Assignment2/Student.Model/Student.cs
@@ -4,13 +4,12 @@ namespace Student.Model;
 public class Student
 {
     public int Id {get; init; }
-    public string GivenName{get; set;}
-    public string Surname{get; set;}
+    public string GivenName{get; set;} = null!;
+    public string Surname{get; set;} = null!;
     public Status Status{get; private set;}
     public DateTime StartDate{get; init;}
     public DateTime EndDate{get; set;}
     public DateTime GraduationDate{get; set;}
-
 
     public Status setStatus()
     {
@@ -39,7 +38,20 @@ public class Student
         return Status.Active;
     }
     throw new ArgumentException("status may have not been initialized proper in student");
-    }}
+    }
+    
+    public override String ToString()
+    {
+    return ("Student number " + Id + "\n"
+     + "Name: " + GivenName + " " + Surname + "\n"
+     + "Status: " + Status + "\n"
+     + "Enrolled: " + StartDate + "\n"
+     + "End: " + EndDate + "\n"
+     + "Graduation: " + GraduationDate + "\n"
+     );
+    }
+    }
 
+    
 
 
