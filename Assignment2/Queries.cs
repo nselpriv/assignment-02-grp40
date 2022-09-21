11b6532368fadcdb9f1a6ed0d760609937a3b7c9
Oscar <oska@itu.dk>
2022-09-21 19:26:32 +0200

    added queries

--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,17 @@ namespace Assignment2;
 
 public class Queries
 {
+    
+    public static IEnumerable<string> GetAllWizardsFromAuthor(string AuthorName)
+    {
+         var wizzy = WizardCollection.Create();
+
+        
+        return   from wizards in wizzy
+                 where wizards.Creator.Contains(AuthorName)
+                 select wizards.Name;
+                 
+
+
+    }
 }
